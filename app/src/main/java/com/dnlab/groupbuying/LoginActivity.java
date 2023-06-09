958c789c525ce3a87c9ff2c67ef96ee19847084f
Hw0o0 <dlgusdn7706@gmail.com>
2023-06-09 11:06:44 +0900

    chore: .gitignore 적용

--- app/src/main/java/com/dnlab/groupbuying/LoginActivity.java
+++ app/src/main/java/com/dnlab/groupbuying/LoginActivity.java
@@ -1,6 +1,7 @@
 package com.dnlab.groupbuying;
 
 import android.app.AlertDialog;
+import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
@@ -9,10 +10,12 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity {
     private EditText edit_id, edit_pw;
-    private Button btn_login, btn_signup;
+    private EditText input_NickName, input_Id, input_Pw;
+    private Button btn_login, btn_signup,btn_chkNickName;
     private SharedPreferences preferences;
 
     @Override
@@ -22,8 +25,12 @@ public class LoginActivity extends AppCompatActivity {
 
         edit_id = findViewById(R.id.edt_id);
         edit_pw = findViewById(R.id.edt_pw);
+        input_NickName = findViewById(R.id.edit_nickname);
+        input_Id = findViewById(R.id.edit_id);
+        input_Pw = findViewById(R.id.edit_pw);
         btn_login = findViewById(R.id.btn_login);
         btn_signup = findViewById(R.id.btn_signup);
+        btn_chkNickName = findViewById(R.id.btn_chk_nickName);
 
         preferences = getSharedPreferences("UserInfo", MODE_PRIVATE);
 
@@ -201,4 +208,5 @@ public class LoginActivity extends AppCompatActivity {
         AlertDialog alertDialog = builder.create();
         alertDialog.show();
     }
+
 }
